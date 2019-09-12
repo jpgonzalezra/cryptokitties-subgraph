21547831bf94e3fe5fadbfc4e8d03768e405ee57
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-09-12 14:18:05 -0300

    initial commit

---
+++ src/SalesAuction.ts
@@ -0,0 +1,37 @@
+import {
+  AuctionCreated as AuctionCreatedEvent,
+  AuctionSuccessful as AuctionSuccessfulEvent,
+  AuctionCancelled as AuctionCancelledEvent,
+} from "../generated/Contract/Contract"
+import {
+  AuctionCreated,
+  AuctionSuccessful,
+  AuctionCancelled,
+} from "../generated/schema"
+
+export function handleAuctionCreated(event: AuctionCreatedEvent): void {
+  let entity = new AuctionCreated(
+    event.transaction.hash.toHex()
+  )
+  entity.tokenId = event.params.tokenId
+  entity.duration = event.params.duration
+  entity.save()
+}
+
+export function handleAuctionSuccessful(event: AuctionSuccessfulEvent): void {
+  let entity = new AuctionSuccessful(
+    event.transaction.hash.toHex()
+  )
+  entity.tokenId = event.params.tokenId
+  entity.totalPrice = event.params.totalPrice
+  entity.winner = event.params.winner
+  entity.save()
+}
+
+export function handleAuctionCancelled(event: AuctionCancelledEvent): void {
+  let entity = new AuctionCancelled(
+    event.transaction.hash.toHex()
+  )
+  entity.tokenId = event.params.tokenId
+  entity.save()
+}
